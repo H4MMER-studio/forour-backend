10f1c6e5bd44a9941e161d8f7a68c325a54f9f08
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-01-03 21:26:16 +0900

    :recycle: Add Aniversary API response example for API Document

--- src/schema/anniversary/response_example/get_multi.py
+++ src/schema/anniversary/response_example/get_multi.py
@@ -4,7 +4,23 @@ anniversary_get_multi_response = {
     "200": {
         "model": GetResponseModel,
         "description": "성공",
-        "content": {"application/json": {"example": {"data": []}}},
+        "content": {"application/json": {"example": {"data": [
+            {
+                "_id": "61c9d790bae9d99ea4c1782c",
+                "name": {
+                    "korean": "우정",
+                    "english": "Solid\nFriendship"
+                },
+                "image": "https://forour.s3.ap-northeast-2.amazonaws.com/Friendship.svg"
+            },
+            {
+                "_id": "61c9d79dbae9d99ea4c1782d",
+                "name": {
+                    "korean": "사랑",
+                    "english": "Eternal\nLove"
+                },
+                "image": "https://forour.s3.ap-northeast-2.amazonaws.com/Love.svg"
+            }]}}},
     },
     "404": {
         "model": GetResponseModel,
